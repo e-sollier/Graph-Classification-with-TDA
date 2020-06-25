5a8f15b8f1d4e052e93021d3358c7420d8a42191
Etienne92 <etienne.sollier@polytechnique.edu>
2020-06-25 10:29:59 +0200

    cleanup

--- run_evaluate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-for dataset in MUTAG PROTEINS NCI1 NCI109 COLLAB IMDB-BINARY IMDB-MULTI REDDIT-BINARY REDDIT-MULTI-5K REDDIT-MULTI-12K; do
-	for filtration in degree node_betweenness; do
-		for dim in 1 2 4; do
-			bsub -n 4 python evaluate.py -d $dataset -f $filtration --dim $dim -o results
-		done
-	done
-done
